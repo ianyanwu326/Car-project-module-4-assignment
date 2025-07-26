17a3f9513db60b4d536111d3de5433eeb9c98c16
ikenna Anyanwu <ianyanwu@cub.uca.edu>
2025-07-26 16:57:58 -0500

    (#ifndef to #define): I created this to  Prevent multiple inclusion of the header.

--- car.h
+++ car.h
@@ -8,12 +8,3 @@ class Car {
 protected:
   string make, model, vin;
   int year;
-public:
-  Car(string make, string model, int year, string vin);
-  virtual ~Car() {}
-  virtual void displayInfo() const = 0;
-  virtual string getType() const = 0;
-};
-
-#endif
-
