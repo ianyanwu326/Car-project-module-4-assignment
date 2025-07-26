47a910a54dc834518144d95423a022ef7033beb2
ikenna Anyanwu <ianyanwu@cub.uca.edu>
2025-07-26 16:05:35 -0500

    I declared the Battery class with private attributes and public constructor and method

--- Battery.h
+++ Battery.h
@@ -4,4 +4,13 @@
 #include <string>
 using namespace std;
 
+class Battery {
+private:
+  int capacityKWh;
+  string brand;
+public:
+  Battery(int capacityKWh = 50, string brand = "Generic");
+  void display() const;
+};
 
+#endif 
