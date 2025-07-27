7c7256d46fed67a4d9c28a9f7ad9b20990f8e331
ikenna Anyanwu <ianyanwu@cub.uca.edu>
2025-07-26 19:52:17 -0500

    #ifndef to #define): Created this to  Prevent multiple inclusion of the header.

--- HybridCar.h
+++ HybridCar.h
@@ -6,13 +6,3 @@
 #include "Battery.h"
 
 class HybridCar : public Car {
-private:
-  Engine engine;
-  Battery battery;
-public:
-  HybridCar(string make, string model, int year, string vin, Engine engine, Battery battery);
-  void displayInfo() const override;
-  string getType() const override;
-};
-
-#endif
