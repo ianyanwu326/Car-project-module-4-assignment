656e96f222f206e938ca646fb1527df110c9a940
ikenna Anyanwu <ianyanwu@cub.uca.edu>
2025-07-26 22:00:36 -0500

    (includes & setup): I  Included necessary headers and initializes dealership and components.

--- main.cpp
+++ main.cpp
@@ -13,18 +13,4 @@ int main() {
   Battery hybridBattery(60, "Panasonic");
   Engine hybridEngine(120, "Hybrid Gas");
 
-  // Create cars with components
-  Car* car1 = new ElectricCar("Tesla", "Model S", 2023, "E123", teslaBattery);
-  Car* car2 = new GasCar("Toyota", "Corolla", 2022, "G456", hondaEngine);
-  Car* car3 = new HybridCar("Ford", "Fusion", 2021, "H789", hybridEngine, hybridBattery);
-
-  // Add cars to dealership
-  dealer.addCar(car1);
-  dealer.addCar(car2);
-  dealer.addCar(car3);
-
-  // Display inventory (polymorphic behavior)
-  dealer.showInventory();
-
-  return 0;
-}
+  
