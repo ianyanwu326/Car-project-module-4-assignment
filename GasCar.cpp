4ac4cdf2bcc1d36fd1bd15858562ddb51a6c82bc
ikenna Anyanwu <ianyanwu@cub.uca.edu>
2025-07-26 19:39:30 -0500

    (methods): I mad an Implemitation  display and type functions specific to gas cars.

--- GasCar.cpp
+++ GasCar.cpp
@@ -7,3 +7,15 @@ GasCar::GasCar(string make, string model, int year, string vin, Engine engine)
   : Car(make, model, year, vin), engine(engine) {
 }
 
+// Displays gas car details
+void GasCar::displayInfo() const {
+  cout << "[Gas Car]" << endl;
+  cout << year << " " << make << " " << model << " (VIN: " << vin << ")" << endl;
+  engine.display();  // Display engine info
+}
+
+// Returns type of car
+string GasCar::getType() const {
+  return "Gas";
+}
+
