e159262a5273df2581af5121cccc5c65d2f30849
ikenna Anyanwu <ianyanwu@cub.uca.edu>
2025-07-26 19:13:12 -0500

    I declares GasCar class inheriting from Car and containing an Engine.

--- GasCar.h
+++ GasCar.h
@@ -7,3 +7,10 @@
 class GasCar : public Car {
 private:
   Engine engine;
+public:
+  GasCar(string make, string model, int year, string vin, Engine engine);
+  void displayInfo() const override;
+  string getType() const override;
+};
+
+#endif
