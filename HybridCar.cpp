d26fd7951138c52a9ebbcf68c7475f28c6044ec9
ikenna Anyanwu <ianyanwu@cub.uca.edu>
2025-07-26 20:19:09 -0500

    (includes & constructor): This includes headers and initializes HybridCar with engine and battery.

--- HybridCar.cpp
+++ HybridCar.cpp
@@ -7,15 +7,3 @@ HybridCar::HybridCar(string make, string model, int year, string vin, Engine eng
   : Car(make, model, year, vin), engine(engine), battery(battery) {
 }
 
-// Displays hybrid car details
-void HybridCar::displayInfo() const {
-  cout << "[Hybrid Car]" << endl;
-  cout << year << " " << make << " " << model << " (VIN: " << vin << ")" << endl;
-  engine.display();    // Show engine info
-  battery.display();   // Show battery info
-}
-
-// Returns type of car
-string HybridCar::getType() const {
-  return "Hybrid";
-}
